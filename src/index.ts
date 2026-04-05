09bf6849b83888372ab7a961a80005f4a6073f8b
Aryan9060 <Aryanofficial9060@gmail.com>
2026-04-05 19:50:27 +0530

    feat: set up basic Express server and update dependencies
    - Added Express as a dependency to handle HTTP requests.
    - Implemented a simple HTTP server in src/index.ts that listens on port 3000.
    - Updated tsconfig.json to include Node types and ESNext libraries for better type support.
    - Added @types/node as a dev dependency for TypeScript type definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import { createServer } from "node:http"
+
+async function main(){
+    try {
+        
+        const server = createServer()
+        const PORT:number = 3000
+
+        server.listen(PORT,()=>{
+            console.log(`Server is running on PORT: ${PORT}`)
+        })
+    } catch (error) {
+        console.log(`Error starting server: ${error}`)
+    }
+}
+
+main()
