4e7b72075c100bf7a06fbfa4c4ef9a03e466a18b
Aryan9060 <Aryanofficial9060@gmail.com>
2026-07-06 16:01:34 +0530

    feat(auth): add JWT authentication and user signin functionality

--- src/app/auth/controller.ts
+++ src/app/auth/controller.ts
@@ -6,6 +6,10 @@ import { userTable } from '../../db/schema.js';
 import { createHmac, randomBytes } from 'node:crypto';
 
 
+const hashPassword = (password: string, salt: string) => {
+    return createHmac('sha256', salt).update(password).digest("hex");
+}
+
 class AuthenticationController {
 
     public async handelSingup(req: Request, res: Response) {
@@ -21,8 +25,28 @@ class AuthenticationController {
 
         const [result] = await db.insert(userTable).values({ firstName, lastName, email, password: hash, salt }).returning({ id: userTable.id })
 
-        return res.status(201).json({message: 'user created successfully', data: result?.id })
+        return res.status(201).json({ message: 'user created successfully', data: result?.id })
 
+
+    }
+
+    public async handelSignin(req: Request, res: Response) {
+        const validateResult = await userSchema.safeParseAsync(req.body);
+        if (validateResult.error) return res.status(400).json({ message: 'body validation failed', error: validateResult.error });
+
+        const { email, password } = validateResult.data;
+        const [userFromDb] = await db.select().from(userTable).where(eq(userTable.email, email));
+        console.log("Existed user --> ", userFromDb);
+
+        if (!userFromDb) return res.status(404).json({ message: `user with email ${email} does not exist` })
+
+        const salt = userFromDb.salt!;
+        const hash = hashPassword(password, salt);
+        if (userFromDb.password !== hash) return res.status(401).json({ message: 'invalid credentials' })
+
+        //TODO : create tocken
+
+        return res.status(200).json({ message: 'user signed in successfully', data: { token: 1 } });
 
     }
 }
--- src/app/auth/routes.ts
+++ src/app/auth/routes.ts
@@ -1,6 +1,12 @@
 import express, { Router } from 'express';
+import AuthenticationController from './controller.js';
+
+const authController = new AuthenticationController();
+
 
 export const authRouter: Router = Router();
 
 
-authRouter.post('/sign-up')
+authRouter.post('/sign-up', authController.handelSingup.bind(authController));
+authRouter.post('/sign-in', authController.handelSignin.bind(authController));
+// authRouter.post('/sign-out')
